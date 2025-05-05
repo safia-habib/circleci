5fa9195c52eeb8e290c347c0a836f21bceb15289
safia-habib <32682322+safia-habib@users.noreply.github.com>
2025-05-05 15:35:25 -0400

    Update buttonclick.test.js

--- test/buttonclick.test.js
+++ test/buttonclick.test.js
@@ -6,7 +6,7 @@ describe("Button Click Test", () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({ headless: true });
     page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    await page.goto("http://0.0.0.0:3000");
   });
 
   afterAll(async () => {
