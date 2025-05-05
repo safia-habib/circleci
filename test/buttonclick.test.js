5e898396459387ec5e8f0d065dc97da803d21204
safia-habib <32682322+safia-habib@users.noreply.github.com>
2025-05-05 13:56:27 -0400

    Create buttonclick.test.js

---
+++ test/buttonclick.test.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+
+describe("Button Click Test", () => {
+  let browser, page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000");
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test("should change text when button is clicked", async () => {
+    await page.click("button");
+    const text = await page.$eval("#displayText", (el) => el.textContent);
+    expect(text).toBe("Hello, Node.js!");
+  });
+});
+
